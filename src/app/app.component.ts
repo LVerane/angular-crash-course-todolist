8a334eb797a824c98ffc2d7cc7c50cf7074d8c69
Lucio Verane <verane64@gmail.com>
2020-06-03 15:53:52 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-crash-todolist';
+  // name: string = 'Lucio';
+
+  constructor() {
+    // console.log(1234);
+    // this.name = 'Ted';
+    // this.changeName('Jhon');
+  }
+
+  // changeName(name: string): void {
+  //   this.name = name;
+  // }
 }
